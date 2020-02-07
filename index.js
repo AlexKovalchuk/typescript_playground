7fd43bb62452abe8a95cc4e1725242293ece1d8f
OleksandrKovalchuk <alexkovalchuk1991@gmail.com>
2020-02-07 16:52:18 +0200

    functions commit

--- index.js
+++ index.js
@@ -1,154 +1,21 @@
-// class Person {
-//   protected name: string;
-//   protected constructor(theName: string) { this.name = theName; }
-// }
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-// // Employee can extend Person
-// class Employee extends Person {
-//   private department: string;
-//   constructor(name: string, department: string) {
-//       super(name);
-//       this.department = department;
-//   }
-//   public getElevatorPitch() {
-//       return `Hello, my name is ${this.name} and I work in ${this.department}.`;
-//   }
-// }
-// let howard = new Employee("Howard", "Sales");
-// let john = new Person("John"); // Error: The 'Person' constructor is protected
-// console.log(howard);
-// let howardElevator = howard.getElevatorPitch();
-// console.log(howardElevator);
-// console.log(john);
-// class Octopus {
-//   readonly name: string;
-//   readonly numberOfLegs: number = 8;
-//   constructor (theName: string) {
-//       this.name = theName;
-//   }
-// }
-// let dad = new Octopus("Man with the 8 strong legs");
-// console.log(dad);
-// dad.name = 'New dad name';
-// console.log(dad.name);
-// class Octopus {
-//   readonly numberOfLegs: number = 8;
-//   constructor(readonly name: string) {
-//   }
-// }
-// class Employee {
-//   fullName: string;
-// }
-// let employee = new Employee();
-// employee.fullName = "Bob Smith";
-// if (employee.fullName) {
-//   console.log(employee.fullName);
-// }
-// class Employee {
-//     private _fullName: string;
-//     private fullNameMaxLength: number = 10;
-//     get fullName(): string {
-//         return this._fullName;
-//     }
-//     set fullName(newName: string) {
-//         if (newName && newName.length > this.fullNameMaxLength) {
-//             throw new Error("fullName has a max length of " + this.fullNameMaxLength);
-//         }
-//         this._fullName = newName;
-//     }
-// }
-// let employee = new Employee();
-// employee.fullName = "Bob Smith";
-// if (employee.fullName) {
-//     console.log(employee.fullName);
-// }
-// console.log('end')
-// class Grid {
-//   static origin = {x: 0, y: 0};
-//   public calculateDistanceFromOrigin = (point: {x: number; y: number;}) => {
-//       let xDist = (point.x - Grid.origin.x);
-//       let yDist = (point.y - Grid.origin.y);
-//       return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
-//   }
-//   constructor (public scale: number) { }
-// }
-// let grid1 = new Grid(1.0);
-// let grid2 = new Grid(5.0);
-var Animal = /** @class */ (function () {
-    function Animal() {
+console.log('Funcs');
+var suits = ["hearts", "spades", "clubs", "diamonds"];
+function pickCard(x) {
+    // Check to see if we're working with an object/array
+    // if so, they gave us the deck and we'll pick the card
+    if (typeof x == "object") {
+        var pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
     }
-    Animal.prototype.move = function () {
-        console.log("roaming the earth...");
-    };
-    return Animal;
-}());
-var Department = /** @class */ (function () {
-    function Department(name) {
-        this.name = name;
+    // Otherwise just let them pick the card
+    else if (typeof x == "number") {
+        var pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
     }
-    Department.prototype.printName = function () {
-        console.log("Department name: " + this.name);
-    };
-    return Department;
-}());
-var AccountingDepartment = /** @class */ (function (_super) {
-    __extends(AccountingDepartment, _super);
-    function AccountingDepartment() {
-        return _super.call(this, "Accounting and Auditing") || this;
-    }
-    AccountingDepartment.prototype.printMeeting = function () {
-        console.log("The Accounting Department meets each Monday at 10am.");
-    };
-    AccountingDepartment.prototype.generateReports = function () {
-        console.log("Generating accounting reports...");
-    };
-    return AccountingDepartment;
-}(Department));
-var department; // ok to create a reference to an abstract type
-// department = new Department(); // error: cannot create an instance of an abstract class
-department = new AccountingDepartment(); // ok to create and assign a non-abstract subclass
-// department.printName();
-// department.printMeeting();
-// department.generateReports();
-var Greeter = /** @class */ (function () {
-    function Greeter(greeting) {
-        this.greeting = greeting || null;
-    }
-    Greeter.prototype.greet = function () {
-        if (this.greeting) {
-            return "Hello, " + this.greeting;
-        }
-        else {
-            return Greeter.standardGreeting;
-        }
-    };
-    Greeter.standardGreeting = "Hello, there";
-    return Greeter;
-}());
-var greeter1;
-greeter1 = new Greeter('Alex');
-console.log(greeter1.greet()); // "Hello, there"
-var greeterMaker = Greeter;
-greeterMaker.standardGreeting = "Hey there!";
-var greeter2 = new greeterMaker(null);
-console.log(greeter2.greet()); // "Hey there!"
-var Point = /** @class */ (function () {
-    function Point() {
-    }
-    return Point;
-}());
-var point3d = { x: 1, y: 2, z: 3 };
-console.log(point3d);
+}
+var myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+var pickedCard1 = myDeck[pickCard(myDeck)];
+console.log("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+var pickedCard2 = pickCard(15);
+console.log("card: " + pickedCard2.card + " of " + pickedCard2.suit);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,169 +1,49 @@
-// class Person {
-//   protected name: string;
-//   protected constructor(theName: string) { this.name = theName; }
-// }
+console.log('Funcs');
 
-// // Employee can extend Person
-// class Employee extends Person {
-//   private department: string;
+let suits = ["hearts", "spades", "clubs", "diamonds"];
 
-//   constructor(name: string, department: string) {
-//       super(name);
-//       this.department = department;
-//   }
-
-//   public getElevatorPitch() {
-//       return `Hello, my name is ${this.name} and I work in ${this.department}.`;
-//   }
-// }
-
-// let howard = new Employee("Howard", "Sales");
-// let john = new Person("John"); // Error: The 'Person' constructor is protected
-// console.log(howard);
-// let howardElevator = howard.getElevatorPitch();
-// console.log(howardElevator);
-// console.log(john);
-
-// class Octopus {
-//   readonly name: string;
-//   readonly numberOfLegs: number = 8;
-//   constructor (theName: string) {
-//       this.name = theName;
-//   }
-// }
-// let dad = new Octopus("Man with the 8 strong legs");
-// console.log(dad);
-// dad.name = 'New dad name';
-// console.log(dad.name);
-
-// class Octopus {
-//   readonly numberOfLegs: number = 8;
-//   constructor(readonly name: string) {
-//   }
-// }
-
-// class Employee {
-//   fullName: string;
-// }
-
-// let employee = new Employee();
-// employee.fullName = "Bob Smith";
-// if (employee.fullName) {
-//   console.log(employee.fullName);
-// }
-
-// class Employee {
-//     private _fullName: string;
-//     private fullNameMaxLength: number = 10;
-//     get fullName(): string {
-//         return this._fullName;
+// function pickCard(x): any {
+//     // Check to see if we're working with an object/array
+//     // if so, they gave us the deck and we'll pick the card
+//     if (typeof x == "object") {
+//         let pickedCard = Math.floor(Math.random() * x.length);
+//         return pickedCard;
 //     }
-
-//     set fullName(newName: string) {
-//         if (newName && newName.length > this.fullNameMaxLength) {
-//             throw new Error("fullName has a max length of " + this.fullNameMaxLength);
-//         }
-        
-//         this._fullName = newName;
+//     // Otherwise just let them pick the card
+//     else if (typeof x == "number") {
+//         let pickedSuit = Math.floor(x / 13);
+//         return { suit: suits[pickedSuit], card: x % 13 };
 //     }
 // }
 
-// let employee = new Employee();
-// employee.fullName = "Bob Smith";
-// if (employee.fullName) {
-//     console.log(employee.fullName);
-// }
-// console.log('end')
+// let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+// console.log('myDeck', myDeck);
+// let pickedCard1 = myDeck[pickCard(myDeck)];
+// console.log('myDeck', myDeck);
+// console.log('pickedCard1:', pickedCard1);
 
-// class Grid {
-//   static origin = {x: 0, y: 0};
-//   public calculateDistanceFromOrigin = (point: {x: number; y: number;}) => {
-//       let xDist = (point.x - Grid.origin.x);
-//       let yDist = (point.y - Grid.origin.y);
-//       return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
-//   }
-//   constructor (public scale: number) { }
-// }
+// let pickedCard2 = pickCard(15);
+// console.log('pickedCard2', pickedCard2);
 
-// let grid1 = new Grid(1.0);
-// let grid2 = new Grid(5.0);
-
-abstract class Animal {
-  abstract makeSound(): void;
-  move(): void {
-      console.log("roaming the earth...");
-  }
+function pickCard(x: {suit: string; card: number; }[]): number;
+function pickCard(x: number): {suit: string; card: number; };
+function pickCard(x): any {
+    // Check to see if we're working with an object/array
+    // if so, they gave us the deck and we'll pick the card
+    if (typeof x == "object") {
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+    // Otherwise just let them pick the card
+    else if (typeof x == "number") {
+        let pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
 }
 
-abstract class Department {
+let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+let pickedCard1 = myDeck[pickCard(myDeck)];
+console.log("card: " + pickedCard1.card + " of " + pickedCard1.suit);
 
-  constructor(public name: string) {
-  }
-
-  printName(): void {
-      console.log("Department name: " + this.name);
-  }
-
-  abstract printMeeting(): void; // must be implemented in derived classes
-  abstract generateReports(): void; // must be implemented in derived classes
-}
-
-class AccountingDepartment extends Department {
-
-  constructor() {
-      super("Accounting and Auditing"); // constructors in derived classes must call super()
-  }
-
-  printMeeting(): void {
-      console.log("The Accounting Department meets each Monday at 10am.");
-  }
-
-  generateReports(): void {
-      console.log("Generating accounting reports...");
-  }
-}
-
-let department: Department; // ok to create a reference to an abstract type
-// department = new Department(); // error: cannot create an instance of an abstract class
-department = new AccountingDepartment(); // ok to create and assign a non-abstract subclass
-// department.printName();
-// department.printMeeting();
-// department.generateReports();
-
-class Greeter {
-  static standardGreeting = "Hello, there";
-  greeting: string;
-  constructor(greeting: string) {
-    this.greeting = greeting || null;
-  }
-  greet() {
-      if (this.greeting) {
-          return "Hello, " + this.greeting;
-      }
-      else {
-          return Greeter.standardGreeting;
-      }
-  }
-}
-
-let greeter1: Greeter;
-greeter1 = new Greeter('Alex');
-console.log(greeter1.greet()); // "Hello, there"
-
-let greeterMaker: typeof Greeter = Greeter;
-greeterMaker.standardGreeting = "Hey there!";
-
-let greeter2: Greeter = new greeterMaker(null);
-console.log(greeter2.greet()); // "Hey there!"
-
-class Point {
-  x: number;
-  y: number;
-}
-
-interface Point3d extends Point {
-  z: number;
-}
-
-let point3d: Point3d = {x: 1, y: 2, z: 3};
-console.log(point3d);
+let pickedCard2 = pickCard(15);
+console.log("card: " + pickedCard2.card + " of " + pickedCard2.suit);
