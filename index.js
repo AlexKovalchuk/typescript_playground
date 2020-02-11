bc10f90a82ec22649f3b7d1da2520afcfbff82ed
OleksandrKovalchuk <alexkovalchuk1991@gmail.com>
2020-02-11 10:28:39 +0200

    rm cnsl

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function isIsogram(str) {
-    //...
     var result = true;
     var length = str.length;
     for (var i = 1; i < length; i++) {
-        console.log(str[i - 1]);
-        console.log(str.slice(i));
         if (str.slice(i).toLowerCase().search(str[i - 1]) >= 0)
             return false;
     }
--- index.ts
+++ index.ts
@@ -1,12 +1,9 @@
 import { assert } from "chai";
 
 function isIsogram(str: string): boolean{
-  //...
   let result = true;
   const length = str.length
   for(let i = 1; i < length; i++) {
-    console.log(str[i-1]);
-    console.log(str.slice(i));
     if(str.slice(i).toLowerCase().search(str[i-1]) >= 0) return false;
   }
 
