a8b582470d0220e45c887761fbb7a1f364befaa4
OleksandrKovalchuk <alexkovalchuk1991@gmail.com>
2020-02-13 13:12:39 +0200

    type compatibilities

--- build/typescript.js
+++ build/typescript.js
@@ -1,2 +1,30 @@
 console.log('hello typescript');
+class Person {
+}
+let p;
+p = new Person();
+let x;
+let y = { name: "Alice", location: "Seattle" };
+x = y;
+console.log('x', x);
+function greet(n) {
+    console.log("Hello, " + n.name);
+}
+function invokeLater(args, callback) {
+    callback(...args);
+}
+invokeLater([1, 2], (x, y) => console.log(x + " && " + y));
+invokeLater([1], (x, y) => console.log(x + " && " + y));
+class Animal {
+    constructor(name, numFeet) {
+        this.name = name;
+        this.feet = numFeet;
+    }
+}
+class Size {
+    constructor(numFeet) { }
+}
+let a = new Animal('Oscar', 4);
+let s;
+console.log(a, s);
 //# sourceMappingURL=typescript.js.map
--- src/scripts/typescript.ts
+++ src/scripts/typescript.ts
@@ -1,1 +1,50 @@
 console.log('hello typescript');
+interface Named {
+  name: string;
+}
+
+class Person {
+  name: string;
+}
+
+let p: Named;
+// OK, because of structural typing
+p = new Person();
+
+
+let x: Named;
+// y's inferred type is { name: string; location: string; }
+let y = { name: "Alice", location: "Seattle" };
+x = y;
+console.log('x', x);
+
+function greet(n: Named) {
+  console.log("Hello, " + n.name);
+}
+
+function invokeLater(args: any[], callback: (...args: any[]) => void) {
+  /* ... Invoke callback with 'args' ... */
+  callback(...args)
+}
+
+invokeLater([1, 2], (x, y) => console.log(x + " && " + y));
+invokeLater([1], (x?, y?) => console.log(x + " && " + y));
+class Animal {
+  feet: number;
+  name: string;
+  constructor(name: string, numFeet: number) {
+    this.name = name;
+    this.feet = numFeet;
+  }
+}
+
+class Size {
+  feet: number;
+  constructor(numFeet: number) { }
+}
+
+let a: Animal = new Animal('Oscar', 4);
+let s: Size;
+console.log(a, s);
+
+
